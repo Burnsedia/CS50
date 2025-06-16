c470548e12588861146a13d6639c52994bd4d189
Burnsedia <bburnsed1995@gmail.com>
2025-06-15 20:27:10 -0400

    tracking my work

--- week1/hello.c
+++ week1/hello.c
@@ -1,10 +1,12 @@
-
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
+    // get user inpput
     string name = get_string("What's your name? ");
+    // output to the terminal
     printf("Hello, %s!\n", name);
+    // return zero for no errors
     return 0;
 }
